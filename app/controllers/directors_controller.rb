161f9c852b852b51f579acb58794e2452d0cf727
Meenoow Castillo <xcastillo33@gmail.com>
2023-04-14 20:15:19 +0000

    New path created for eldest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,8 +1,14 @@
 class DirectorsController < ApplicationController
 
   def index
+    @list_of_directors = Director.all
 
-  render({ :template => "directors_template/index.html.erb"})
+   render({ :template => "directors_template/index.html.erb"})
+  end
+
+  def oldest
+
+   render({ :template => "directors_template/eldest.html.erb"})  
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
+  get("/directors/eldest", { :controller => "directors", :action => "oldest" })
+
   get("/directors", { :controller => "directors", :action => "index" })
 end
