a5231388094a09202ab90e9d44ce4739d9a1ac20
Meenoow Castillo <xcastillo33@gmail.com>
2023-04-16 16:41:02 +0000

    Director page showing image and filmography on a table

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "application", :action => "homepage" })
-
+  get("/directors/eldest", { :controller => "directors", :action => "oldest" })
   get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 
-  get("/directors/eldest", { :controller => "directors", :action => "oldest" })
-  #get("/directors/eldest", { :controller => "directors", :action => "oldest" })
+  get("/", { :controller => "application", :action => "homepage" })
+  
+  get("/directors/show", { :controller => "directors", :action => "director_details" })
 
   get("/directors", { :controller => "directors", :action => "index" })
 end
